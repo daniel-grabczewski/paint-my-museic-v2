69701d4c8f5238e32fe342c6b1e81d5dac0e8c1d
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-09 15:33:42 +1200

    Implement mouse move listener for drawing

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,27 +1,41 @@
-import { useRef } from 'react'
+import { useRef } from "react";
 
 export function useOnDraw(onDraw) {
-  const canvasRef = useRef(null)
-  const isDrawingRef = useRef(false)
-  const prevPointRef = useRef(null)
+  const canvasRef = useRef(null);
+  const isDrawingRef = useRef(false);
+  const prevPointRef = useRef(null);
 
   function computePointInCanvas(clientX, clientY) {
     if (canvasRef.current) {
-      const boundingRect = canvasRef.current.getBoundingClientRect()
+      const boundingRect = canvasRef.current.getBoundingClientRect();
       return {
         x: clientX - boundingRect.left,
-        y: clientY - boundingRect.top,
+        y: clientY - boundingRect.top
       }
     } else {
-      return null
+      return null;
     }
   }
 
   function onCanvasMouseDown() {
-    isDrawingRef.current = true
+    isDrawingRef.current = true;
+    // Continue expanding this function in subsequent commits
   }
 
+  function initMouseMoveListener() {
+    const mouseMoveListener = (e) => {
+      if (isDrawingRef.current && canvasRef.current) {
+        const point = computePointInCanvas(e.clientX, e.clientY);
+        // Drawing logic will be added later
+      }
+    };
+    window.addEventListener("mousemove", mouseMoveListener);
+  }
+
+  initMouseMoveListener();
+
   return {
-    onCanvasMouseDown,
+    onCanvasMouseDown
   }
 }
+
