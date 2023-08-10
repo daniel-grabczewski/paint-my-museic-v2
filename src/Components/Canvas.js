331396e4c1a33aed9dce3e0c1f6b203e0605264a
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-10 15:06:46 +1200

    Implement onDraw and drawLine functions for drawing on canvas

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -3,14 +3,33 @@ import { useState, useRef, useEffect } from 'react'
 import { colors } from '../data'
 
 const Canvas = ({ width, height }) => {
-  const [color, setColor] = useState(colors[3]); 
+  const [color, setColor] = useState(colors[3]);
   const audioRef = useRef(new Audio(color.music));
+  const ctxRef = useRef(null);
 
   useEffect(() => {
     audioRef.current.src = color.music;
   }, [color]);
 
-  const { onCanvasMouseDown } = useOnDraw();
+  const { onCanvasMouseDown } = useOnDraw(
+    onDraw,
+    audioRef,
+    ctxRef
+  );
+
+  function onDraw(ctx, point, prevPoint) {
+    drawLine(prevPoint, point, ctx, color.code, 5);
+  }
+
+  function drawLine(start, end, ctx, color, width) {
+    start = start ?? end;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
+  }
 
   return (
     <canvas
