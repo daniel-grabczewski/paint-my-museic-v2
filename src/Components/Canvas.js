abdc67aef75cc3f29e0bdb888121cea67c16a3ea
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-10 14:25:15 +1200

    Add color state and audio references to Canvas

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,11 +1,21 @@
+import { useOnDraw } from './Hooks'
+import { useState, useRef } from 'react'
+import { colors } from '../data'
+
 const Canvas = ({ width, height }) => {
+  const [color, setColor] = useState(colors[3]); // The initial color is black
+  const audioRef = useRef(new Audio(color.music));
+
+  const { onCanvasMouseDown } = useOnDraw();
+
   return (
     <canvas
       width={width}
       height={height}
+      onMouseDown={onCanvasMouseDown}
       style={{ border: '1px solid black' }}
     />
-  )
+  );
 }
 
-export default Canvas
+export default Canvas;
