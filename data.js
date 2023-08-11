01b582fe07534754ffb732ac4bd92a08367a73ca
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-11 12:34:07 +1200

    add color data array

---
+++ data.js
@@ -0,0 +1,44 @@
+export const colors = [
+  {
+    color: 'red',
+    code: '#A71814',
+    music: '/music/red.mp3',
+    isPicked: false,
+    image: '/images/red.jpg',
+  },
+  {
+    color: 'blue',
+    code: '#383686',
+    music: '/music/blue.mp3',
+    isPicked: false,
+    image: '/images/blue.jpg',
+  },
+  {
+    color: 'green',
+    code: '#88C752',
+    music: '/music/green.mp3',
+    isPicked: false,
+    image: '/images/green.jpg',
+  },
+  {
+    color: 'black',
+    code: '#181818',
+    music: '/music/black.mp3',
+    isPicked: false,
+    image: '/images/black.jpg',
+  },
+  {
+    color: 'pink',
+    code: '#E664bf',
+    music: '/music/pink.mp3',
+    isPicked: false,
+    image: '/images/pink.jpg',
+  },
+  {
+    color: 'white',
+    code: 'white',
+    music: '/music/white.mp3',
+    isPicked: false,
+    image: '/images/eraser.jpg',
+  },
+]
