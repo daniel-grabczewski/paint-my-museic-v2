0dcc57beb918fdda576229fd57e1e8c2884cc9aa
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-24 10:11:00 +1300

    update model for onDrawFunc

--- client/models.ts
+++ client/models.ts
@@ -1,23 +1,22 @@
 export type PointType = {
-  x: number;
-  y: number;
-};
-
-export interface OnDrawFunc {
-  (
-    ctx: CanvasRenderingContext2D,
-    point: PointType,
-    prevPoint: PointType | null
-  ): void;
+  x: number
+  y: number
 }
 
+export type OnDrawFunc = (
+  ctx: CanvasRenderingContext2D,
+  p1: PointType,
+  p2: PointType,
+  p3: PointType
+) => void
+
 export type ColorType = {
-  color: string;
-  code: string;
-  music: string;
-};
+  color: string
+  code: string
+  music: string
+}
 
 export interface CanvasProps {
-  width: number;
-  height: number;
+  width: number
+  height: number
 }
